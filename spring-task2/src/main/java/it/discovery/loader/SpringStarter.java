c793a77743a5d7d615c00c4f369e0b6cc970d91a
Sergey <sergey.morenets@yandex.ua>
2019-03-16 12:56:07 +0200

    Task #4

--- spring-task2/src/main/java/it/discovery/loader/SpringStarter.java
+++ spring-task2/src/main/java/it/discovery/loader/SpringStarter.java
@@ -1,6 +1,8 @@
 package it.discovery.loader;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,25 +10,32 @@ import it.discovery.model.Book;
 import it.discovery.service.BookService;
 
 public class SpringStarter {
-	public static void main(String[] args) {
-		try (ClassPathXmlApplicationContext context =
+    public static void main(String[] args) {
+        try (ClassPathXmlApplicationContext context =
                      new ClassPathXmlApplicationContext("spring-beans.xml")) {
-			
-			BookService service = context.getBean(BookService.class);
-			//service = context.getBean("service");
-			
-			Book book = new Book();
-			book.setName("Introduction into Spring");
-			book.setPages(100);
-			book.setYear(2016);
-			service.saveBook(book);
 
-			List<Book> books = service.findBooks();
-			System.out.println(books);
+            BookService service = context.getBean(BookService.class);
+            //service = context.getBean("service");
 
-            System.out.println("Total bean count " + context.getBeanDefinitionCount());
-		}
+            Book book = new Book();
+            book.setName("Introduction into Spring");
+            book.setPages(100);
+            book.setYear(2016);
+            service.saveBook(book);
 
-	}
+            List<Book> books = service.findBooks();
+            System.out.println(books);
+
+            System.out.println("Total bean count: " + context.getBeanDefinitionCount());
+            System.out.println("Our beans: " +
+                    Arrays.stream(context.getBeanDefinitionNames())
+                            .map(context::getBean)
+                            .map(Object::getClass)
+                            .map(Class::getSimpleName)
+                            .collect(Collectors.joining(",")))
+            ;
+        }
+
+    }
 
 }
--- spring-task2/src/main/java/it/discovery/service/BookServiceImpl.java
+++ spring-task2/src/main/java/it/discovery/service/BookServiceImpl.java
@@ -2,6 +2,7 @@ package it.discovery.service;
 
 import it.discovery.model.Book;
 import it.discovery.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -9,11 +10,19 @@ public class BookServiceImpl implements BookService {
 
 	private final BookRepository repository;
 
+	private List<BookRepository> repositories;
+
+	@Autowired
 	public BookServiceImpl(BookRepository repository) {
 		this.repository = repository;
 		System.out.println("Using db repository");
 	}
-	
+
+	@Autowired
+	public void setRepositories(List<BookRepository> repositories) {
+		this.repositories = repositories;
+	}
+
 	@Override
 	public void saveBook(Book book) {
 		repository.saveBook(book);
@@ -27,5 +36,9 @@ public class BookServiceImpl implements BookService {
 	@Override
 	public List<Book> findBooks() {
 		return repository.findBooks();
+	}
+
+	public List<BookRepository> getRepositories() {
+		return repositories;
 	}
 }
