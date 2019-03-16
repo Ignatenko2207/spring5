54d910c7ce9442da8600da2c0bef836b4425f762
Sergey <sergey.morenets@yandex.ua>
2019-03-16 17:23:09 +0200

    Task #9.2 Use Environment for property injection

--- spring-task2/src/main/java/it/discovery/config/AppConfig.java
+++ spring-task2/src/main/java/it/discovery/config/AppConfig.java
@@ -6,10 +6,10 @@ import it.discovery.repository.XMLBookRepository;
 import it.discovery.service.BookService;
 import it.discovery.service.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("application.properties")
@@ -23,8 +23,9 @@ public class AppConfig {
 
     @Bean
     @Qualifier("db")
-    public BookRepository dbRepository(@Value("${book.server}") String server, @Value("${book.db}") String db) {
-        return new DBBookRepository(server, db);
+    public BookRepository dbRepository(Environment env) {
+        return new DBBookRepository(env.getProperty("book.server", "localhost"),
+                env.getProperty("book.db", "library"));
     }
 
     @Bean
